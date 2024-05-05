46a6dc99c5b7cc09ec05bab512ec049cce0333b7
Raisul <raisulislamshanto24.2@gmail.com>
2024-05-05 17:47:47 +0600

    assignment commited first time

---
+++ myscript.js
@@ -0,0 +1,11 @@
+let day = "sunday";
+var week = "first week";
+const month = "1st month";
+
+alert("Raisul Islam shanto");
+console.log("Raisul Islam Shanto");
+
+const first_number = 20;
+const second_number = 30;
+var summation = first_number + second_number;
+console.log(summation);
